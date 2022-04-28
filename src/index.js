30a2b605ffd08b066e35a5eb21d57f97529c8bd4
amarynets <andrii.marynets@gmail.com>
2022-04-28 14:47:21 +0300

    Fix path in bin

--- src/index.js
+++ src/index.js
@@ -11,14 +11,9 @@ const getFileFormat = (filePath) => path.extname(filePath).slice(1);
 export default (filepath1, filepath2, formater = 'stylish') => {
   const leftFile = readFile(filepath1);
   const rightFile = readFile(filepath2);
-  console.log(filepath1, filepath2, formater);
   const left = parse(leftFile, getFileFormat(filepath1));
   const right = parse(rightFile, getFileFormat(filepath2));
-  
   const diffRes = genDiff(left, right);
-  console.log(left);
-  console.log(right);
-  console.log(diffRes);
   const diff = format(diffRes, formater);
   return diff;
 };
