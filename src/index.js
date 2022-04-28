51b3cfb55cdb86244497715b3ae9f20464e56d20
amarynets <andrii.marynets@gmail.com>
2022-04-28 13:36:44 +0300

    Add resolve to path

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import parse from './parsers.js';
 import format from './formaters/index.js';
 import genDiff from './genDiff.js';
 
-const readFile = (filePath) => readFileSync(filePath);
+const readFile = (filePath) => readFileSync(path.resolve(filePath));
 
 const getFileFormat = (filePath) => path.extname(filePath).slice(1);
 
 export default (filepath1, filepath2, formater = 'stylish') => {
   const leftFile = readFile(filepath1);
   const rightFile = readFile(filepath2);
-  console.log(filepath1, filepath2, format);
+  console.log(filepath1, filepath2, formater);
   const left = parse(leftFile, getFileFormat(filepath1));
   const right = parse(rightFile, getFileFormat(filepath2));
   const diffRes = genDiff(left, right);
